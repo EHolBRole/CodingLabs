59bfa7dc46dcb569a96ac76a3b192f8e52020cfc
EHolBRole <52851286+EHolBRole@users.noreply.github.com>
2022-10-26 18:24:13 +0300

    5565

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -17,7 +17,7 @@ def is_prime(n: int):
         return False
     if n == 2:
         return True
-    for i in range(2, n // 2):
+    for i in range(1, n // 2):
         if n % i == 0:
             return False
     return True
