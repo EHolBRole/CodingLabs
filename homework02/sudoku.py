7b6d754f64f7b1e7a8bef2809bfbde63889b6824
EHolBRole <52851286+EHolBRole@users.noreply.github.com>
2023-01-10 21:21:25 +0300

    chg

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -45,7 +45,7 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     """
     matrix = list()
     counter = 0
-    row: tp.List[str]
+    row: tp.List[T]
     row = list()
     n = n
     for el in values:
@@ -192,7 +192,7 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
     pos = find_empty_positions(grid)
-    a = (-1, -1)
+    a = -1, -1
     if pos != a:
         posval = find_possible_values(grid, pos)
         for val in posval:
@@ -202,7 +202,7 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
             if pos2 == a:
                 return grid
         ala = find_empty_positions(grid)
-        a = (-1, -1)
+        a = -1, -1
         if ala != a:
             grid[pos[0]][pos[1]] = "."
         return grid
