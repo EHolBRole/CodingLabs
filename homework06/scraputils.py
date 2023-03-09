697d28d6cdfcb82e2093e084d611a74982651e67
EHolBRole <ekholo.banderole@mail.ru>
2023-03-09 16:39:20 +0300

    scrapper done

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -7,12 +7,26 @@ def extract_news(parser):
     news_list = []
 
     # PUT YOUR CODE HERE
-
+    html = parser.table.findAll("table")[1].findAll("tr")
+    for i in range(0, len(html), 3):
+        try:
+            current_news = {
+                "title": html[i].findAll("a")[1].string,
+                "author": html[i + 1].find("a").string,
+                "url": html[i].findAll("a")[1].get("href"),
+                "points": int(html[i + 1].findAll("span")[1].string.split()[0]),
+                "comments": html[i + 1].findAll("a")[-1].string.split()[0],
+            }
+            news_list.append(current_news)
+        except IndexError:
+            continue
     return news_list
 
 
 def extract_next_page(parser):
     """ Extract next page URL """
+    url = parser.table.findAll("table")[1].findAll("tr")[-1].contents[2].find("a").get("href")
+    return url
     # PUT YOUR CODE HERE
 
 
